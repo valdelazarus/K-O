88d2fc8101c75bb5396d5552688ea4369c663935
Reneton-Work-PC <reneton13@hotmail.com>
2020-02-07 20:20:51 -0500

    Added Bootleg Dragon. Needs Severe Work

---
+++ Assets/Rene Folder/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Rene Folder/Respawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Sphere;
+    public float spawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnBall", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnBall()
+    {
+   
+    }
+}
