b73d9ec38a5ab51ebbf1ab99a296b3131455377b
Ha Nguyen <valdelazarus@hotmail.com>
2020-02-10 03:21:39 -0500

    Implemented hazard spawner

---
+++ Assets/Hazards/Hazard Scripts/HazardSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardSpawner : MonoBehaviour
+{
+    public Transform[] spawnPositions;
+    public GameObject[] hazardPrefabs;
+    public float initialDelay;
+    public float spawnRate; 
+
+    void Start()
+    {
+        InvokeRepeating("SpawnHazard", initialDelay, spawnRate);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void SpawnHazard()
+    {
+        int rand = Random.Range(0, hazardPrefabs.Length);
+        int randPos = Random.Range(0, spawnPositions.Length);
+
+        GameObject hazard = Instantiate(hazardPrefabs[rand], spawnPositions[randPos].position, Quaternion.identity);
+        Destroy(hazard, spawnRate);
+    }
+}
