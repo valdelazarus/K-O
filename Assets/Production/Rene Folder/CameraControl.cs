98da7e6acbd02c4de1d3dfbb1e754779e4f886a2
Reneton-Work-PC <reneton13@hotmail.com>
2020-03-30 20:12:53 -0400

    Hazard2
    Nuclear Waste/RadioActive  Barrows might be the reason why Zombies and monkeys are going crazy

---
+++ Assets/Production/Rene Folder/CameraControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+ public Camera rb1;
+    public Camera rb2;
+    public Camera rb3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb1.enabled = true;
+        rb2.enabled = false;
+        rb3.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            rb1.enabled = true;
+            rb2.enabled = false;
+            rb3.enabled = false;
+        }  else if (Input.GetKeyDown("s"))
+        {
+            rb1.enabled = false;
+            rb2.enabled = true;
+            rb3.enabled = false;
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            rb1.enabled = false;
+            rb2.enabled = false;
+            rb3.enabled = true;
+        }
+
+
+    }
+}
